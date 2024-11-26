1f8a4977b4e92ec87de29ddd6791314259078819
47Mags47 <gord_ka47@mail.ru>
2024-11-26 17:14:52 +0700

    Добавлен вывод ошибок на странице package.data.index

--- resources/views/pages/payment/data/index.blade.php
+++ resources/views/pages/payment/data/index.blade.php
@@ -14,6 +14,7 @@
                 <x-table.hcell title="ДР" />
                 <x-table.hcell title="КБК" />
                 <x-table.hcell title="СНИЛС" />
+                <x-table.hcell title="Ошибки" />
             </x-table.row>
         </x-slot:thead>
         <x-slot:tbody>
@@ -28,6 +29,13 @@
                     <x-table.cell :title="$row->birth->format('d.m.Y')" />
                     <x-table.cell :title="$row->kbk" />
                     <x-table.cell :title="$row->snils" />
+                    <x-table.cell>
+                        <x-list.box>
+                            @foreach ($row->errors ?? [] as $error)
+                                <x-list.item :title="$error" />
+                            @endforeach
+                        </x-list.box>
+                    </x-table.cell>
                 </x-table.row>
             @endforeach
         </x-slot:tbody>
