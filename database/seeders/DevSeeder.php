a5337a2a9a6d220913d83a4945cdd614a6a25a40
47Mags47 <gord_ka47@mail.ru>
2024-11-26 16:12:18 +0700

    Добавление DevSeeder

---
+++ database/seeders/DevSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Main\CalendarEvent;
+use App\Models\Main\Package;
+use App\Models\Main\PackageData;
+use App\Models\Main\PackageFile;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DevSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        CalendarEvent::factory()
+            ->has(Package::factory()->count(3)->has(
+                PackageFile::factory()->count(3)->has(
+                    PackageData::factory()->count(3),
+                    'data'
+                ),
+                'files'
+            ))
+            ->create();
+    }
+}
