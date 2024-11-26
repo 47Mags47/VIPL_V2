d032e005dba8b78d270dca056ec0c4f9df8c5d4f
47Mags47 <gord_ka47@mail.ru>
2024-11-26 16:11:56 +0700

    fixs

--- routes/web/calendar.php
+++ routes/web/calendar.php
@@ -4,12 +4,12 @@ use App\Http\Controllers\web\calendar\CalendarController;
 use App\Http\Controllers\web\calendar\EventController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('event')->controller(EventController::class)->group(function () {
-    Route::get('/index', 'index')->name('calendar.event.index');
-    Route::get('/create', 'create')->name('calendar.event.create');
-    Route::post('/store', 'store')->name('calendar.event.store');
-});
-
 Route::prefix('calendar')->controller(CalendarController::class)->group(function () {
     Route::get('/index/{year?}/{month?}', 'index')->name('calendar.index');
+
+    Route::prefix('event')->controller(EventController::class)->group(function () {
+        Route::get('/index', 'index')->name('calendar.event.index');
+        Route::get('/create', 'create')->name('calendar.event.create');
+        Route::post('/store', 'store')->name('calendar.event.store');
+    });
 });
